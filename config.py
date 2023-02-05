6fb127de3e34c37545b6a7475b93f325ddf61057
SYTHON <124038843+sythontm@users.noreply.github.com>
2023-02-05 13:51:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,12 @@ from telethon.sessions import StringSession
 import os
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
-
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
 session = os.environ.get("TERMUX")
 SESSION = os.environ.get("TERMUX")
-
-sython = TelegramClient(StringSession(session), APP_ID, APP_HASH)
+token = os.environ.get("TOKEN")
+fifthon = TelegramClient(StringSession(session), APP_ID, APP_HASH)
+bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 ispay = ['yes']
 ispay2 = ['yes']
-
+bot.start()
